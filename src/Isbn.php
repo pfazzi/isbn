e134a8e8a44b3bc0ee53fb2a13979ba75cc982db
Patrick Luca Fazzi <patrick91@live.it>
2019-05-13 21:19:12 +0200

    Initial commit

---
+++ src/Isbn.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace Pfazzi\Isbn;
+
+use Assert\Assertion;
+
+final class Isbn
+{
+    /**
+     * @ORM\Column(length=13)
+     *
+     * @var string
+     */
+    private $value;
+
+    private function __construct(string $value)
+    {
+        Assertion::regex($value, '/^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/');
+
+        $this->value = $value;
+    }
+
+    public static function fromString(string $isbn): self
+    {
+        return new self($isbn);
+    }
+
+    public function __toString()
+    {
+        return $this->value;
+    }
+
+    public function toString(): string
+    {
+        return (string) $this;
+    }
+
+    public function equals(self $other): bool
+    {
+        return $this->value === $other->value;
+    }
+}
